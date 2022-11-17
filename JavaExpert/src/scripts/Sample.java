b203628218147bae500e4769de5ec0ba1d758411
Valmiki Vishwakarma <you@example.com>
2022-11-18 00:39:57 +0530

    code changes.

---
+++ JavaExpert/src/scripts/Sample.java
@@ -0,0 +1,15 @@
+package scripts;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		String code = "100";
+		int number = Integer.parseInt(code);
+
+		System.out.println(number);
+		//System.out.println(instanceof(number));
+
+	}
+
+}
---
+++ JavaExpert/src/scripts/T1293.java
@@ -0,0 +1,13 @@
+package scripts;
+
+public class T1293 {
+
+	public static void main(String[] args) {
+	
+		String str="BlackFriday";
+		
+		for(Character chr:str.toCharArray()) {
+			System.out.println(chr);
+		}
+	}
+}
